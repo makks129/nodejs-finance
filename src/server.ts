c557bb30d42dcfb5a259f2a9c95a8beb9cef1d20
Maxim Strelnikov <maxim@speakly.me>
2021-03-18 18:24:07 +0200

    Added Docker (WIP); Added first src logic (WIP)

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// import Logger from './core/Logger';
+import { port } from './config';
+import app from './app';
+
+app
+  .listen(port, () => {
+    // Logger.info(`server running on port : ${port}`);
+    console.log(`server running on port : ${port}`);
+  })
+  .on('error', (e: any) => {
+    // Logger.error(e);
+    console.log(e);
+  });
